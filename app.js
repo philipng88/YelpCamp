a89a09c28a0d47860b54b2cf7849b1128d091166
philipng88 <philiplyng@gmail.com>
2019-06-19 16:59:58 -0600

    implemented comments features and styling

--- app.js
+++ app.js
@@ -2,16 +2,20 @@ const express = require("express")
 const bodyParser = require("body-parser")
 const mongoose = require("mongoose")
 const Campground = require("./models/campground")
+const Comment = require("./models/comment")
+const seedDB = require('./seeds')
 const app = express()
 const port = 3000
 
 app.listen(port, () => {
     console.log("The YelpCamp server has started") 
 })
-
+ 
 mongoose.connect("mongodb://localhost/yelp_camp", { useNewUrlParser: true })
 app.use(bodyParser.urlencoded({extended: true}))
 app.set("view engine", "ejs")
+app.use(express.static(__dirname + "/public")) 
+seedDB()
 
 app.get("/", (req, res) => {
     res.render("landing")
@@ -22,7 +26,7 @@ app.get("/campgrounds", (req, res) => {
         if(err) {
             console.log(err)
         } else {
-            res.render("index", {campgrounds:allCampgrounds})
+            res.render("campgrounds/index", {campgrounds:allCampgrounds})
         }
     })
 })
@@ -42,15 +46,44 @@ app.post("/campgrounds", (req, res) => {
 }) 
 
 app.get("/campgrounds/new", (req, res) => {
-    res.render("new.ejs") 
+    res.render("campgrounds/new") 
 })
 
 app.get("/campgrounds/:id", (req, res) => {
-    Campground.findById(req.params.id, (err, foundCampground) => {
+    Campground.findById(req.params.id).populate("comments").exec((err, foundCampground) => {
         if(err) {
             console.log(err)
         } else {
-            res.render("show", {campground: foundCampground}) 
+            res.render("campgrounds/show", {campground: foundCampground}) 
         }
     }) 
+})
+
+app.get("/campgrounds/:id/comments/new", (req, res) => {
+    Campground.findById(req.params.id, (err, campground) => {
+        if(err) {
+            console.log(err)
+        } else {
+            res.render("comments/new", {campground: campground}) 
+        }
+    })
+})
+
+app.post("/campgrounds/:id/comments", (req, res) => {
+    Campground.findById(req.params.id, (err, campground) => {
+        if(err) {
+            console.log(err)
+            res.redirect("/campgrounds")
+        } else {
+            Comment.create(req.body.comment, (err, comment) => {
+                if(err) {
+                    console.log(err)
+                } else {
+                    campground.comments.push(comment)
+                    campground.save()
+                    res.redirect("/campgrounds/" + campground._id) 
+                }
+            })
+        }
+    })
 })
---
+++ models/comment.js
@@ -0,0 +1,7 @@
+const mongoose = require("mongoose")
+let commentSchema = new mongoose.Schema({
+    text: String,
+    author: String 
+})
+
+module.exports = mongoose.model("Comment", commentSchema)
